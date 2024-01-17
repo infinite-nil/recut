6478b8d183d448debf1a0f916e25a5dc7ccbd8c8
João Alberto <hey@joaoalberto.dev>
2024-01-17 19:34:01 -0300

    fix: prettier and tailwind

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
