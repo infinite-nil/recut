2e01300ebdfa8a0e61b26ffa6500583078f3a562
João Alberto <hey@joaoalberto.dev>
2024-01-20 02:55:53 -0300

    chore: move postcss config to dir

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  css: {
+    postcss: './config/postcss.config.js',
+  },
   plugins: [react()],
   test: {
     environment: 'jsdom',
